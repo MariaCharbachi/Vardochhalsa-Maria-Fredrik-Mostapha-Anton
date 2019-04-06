bde933f7040fb6c5647caece315c4ddba49bb499
MariaCharbachi <42807821+MariaCharbachi@users.noreply.github.com>
2019-04-06 18:10:28 +0200

    Add files via upload

---
+++ lanskoder.js
@@ -0,0 +1,92 @@
+
+lansKoder =
+[{
+    "befolkning 20161231": "152757",
+    "namn": "Blekinge",
+    "läns-/kommunkod": "10"
+}, {
+    "befolkning 20161231": "277349",
+    "namn": "Dalarna",
+    "läns-/kommunkod": "20"
+}, {
+    "befolkning 20161231": "57161",
+    "namn": "Gotland",
+    "läns-/kommunkod": "09"
+}, {
+    "befolkning 20161231": "277970",
+    "namn": "Gävleborg",
+    "läns-/kommunkod": "21"
+}, {
+    "befolkning 20161231": "306840",
+    "namn": "Halland",
+    "läns-/kommunkod": "13"
+}, {
+    "befolkning 20161231": "126461",
+    "namn": "Jämtland",
+    "läns-/kommunkod": "23"
+}, {
+    "befolkning 20161231": "341235",
+    "namn": "Jönköping",
+    "läns-/kommunkod": "06"
+}, {
+    "befolkning 20161231": "233874",
+    "namn": "Kalmar",
+    "läns-/kommunkod": "08"
+}, {
+    "befolkning 20161231": "187156",
+    "namn": "Kronoberg",
+    "läns-/kommunkod": "07"
+}, {
+    "befolkning 20161231": "249436",
+    "namn": "Norrbotten",
+    "läns-/kommunkod": "25"
+}, {
+    "befolkning 20161231": "9644864",
+    "namn": "RIKET",
+    "läns-/kommunkod": ""
+}, {
+    "befolkning 20161231": "1274069",
+    "namn": "Skåne",
+    "läns-/kommunkod": "12"
+}, {
+    "befolkning 20161231": "345481",
+    "namn": "Uppsala",
+    "läns-/kommunkod": "03"
+}, {
+    "befolkning 20161231": "2163042",
+    "namn": "Stockholm",
+    "läns-/kommunkod": "01"
+}, {
+    "befolkning 20161231": "277569",
+    "namn": "Sörmland",
+    "läns-/kommunkod": "04"
+}, {
+    "befolkning 20161231": "273815",
+    "namn": "Värmland",
+    "läns-/kommunkod": "17"
+}, {
+    "befolkning 20161231": "261112",
+    "namn": "Västerbotten",
+    "läns-/kommunkod": "24"
+}, {
+    "befolkning 20161231": "242156",
+    "namn": "Västernorrland",
+    "läns-/kommunkod": "22"
+}, {
+    "befolkning 20161231": "259054",
+    "namn": "Västmanland",
+    "läns-/kommunkod": "19"
+}, {
+    "befolkning 20161231": "1615084",
+    "namn": "Västra Götaland",
+    "läns-/kommunkod": "14"
+}, {
+    "befolkning 20161231": "285395",
+    "namn": "Örebro",
+    "läns-/kommunkod": "18"
+}, {
+    "befolkning 20161231": "437848",
+    "namn": "Östergötland",
+    "läns-/kommunkod": "05"
+}
+]
